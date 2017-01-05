0bd805e6fb8f2c40e10f119b0b4c1846a73efb10
jmarkim <jmarkim.gt@hotmail.com>
2017-01-05 14:55:34 -0200

    Refatoração do código
    Adição de linhas do Grid:
       Preta -> Quadros
       Vermelha -> Largura de um compasso
       Azul -> Largura de uma tela
    
    Mudança nas cores do bakcground para melhorar contraste
       Rosa -> Áreas fixas (início e fim de fase)
       Lilás -> Área gerada pela músca

--- Music2Game-MapMaker/Program.cs
+++ Music2Game-MapMaker/Program.cs
@@ -9,11 +9,14 @@ using System.Drawing.Imaging;
 
 namespace Music2Game_MapMaker {
     class Program {
+
+        // Constantes
+        private static int BASEHEIGHT = 10; // Altura da primeira tela;
+        private static int SCREEN = 32; // Tamanho em quadros da tela
+        private static int MEASURE = 16; // Tamanho em quadros de um compasso
+        private static int GRID = 5; // Lados do quadro do grid (em px)
+
         static void Main(string[] args) {
-            const int SCREEN = 32; // Tamanho em quadros da tela
-            const int MEASURE = 16; // Tamanho em quadros de um compasso
-            const int BASEHEIGHT = 10; // Altura da primeira tela;
-            const int GRID = 5; // Lados do quadro do grid (em px)
 
             // Representação da partitura
             Score music = ScoreBuilder.FromXML(args[0]);
@@ -22,13 +25,23 @@ namespace Music2Game_MapMaker {
             string name = args[0].Remove(args[0].Length - 4);
 
             // Sequencia de alturas das "telas"
-            List<int> heights = new List<int>(music.Parts[0].Measures.Count + 4);
+            List<int> heights = SetHeights(music);
+
+            // Desanha mapa, como definido pela sequência de alturas
+            DrawMap(name, heights);
+            
+        }
+
+        public static List<int> SetHeights(Score music) {
+
+            int measureCount = music.Parts[0].Measures.Count; // Total de compassos da música
+            int partCount = music.Parts.Count; // Total de partes (instrumentos) da música
+            List<int> heights = new List<int>(measureCount + 4);
+
             heights.Add(BASEHEIGHT); // Primeira tela 1 de 2
             heights.Add(BASEHEIGHT); // Priemira tela 2 de 2
 
             // Auxiliares para o loop
-            int measureCount = music.Parts[0].Measures.Count; // Total de compassos da música
-            int partCount = music.Parts.Count; // Total de partes (instrumentos) da música
             Measure msr;
             int delta = 0; // Contibuição da parte
             int sum = 0; // Soma dos delta
@@ -91,30 +104,79 @@ namespace Music2Game_MapMaker {
                     }
                     sum += (5 * delta) / msr.Size;
                 }
-                heights.Add(heights.Last() + sum);
+                heights.Add(heights.Last() + sum / partCount);
+                //Console.WriteLine(heights.Last());
             }
 
             heights.Add(heights.Last()); // Área final 1 de 2
             heights.Add(heights.Last()); // Área final 2 de 2
 
+            return heights;
+        }
+
+        public static void DrawMap(string name, List<int> heights) {
+            
             // Auxiliares para a imagem
             int width = heights.Count * MEASURE * GRID; // Largura da imagem gerada
             int height = (heights.Max() + 2 * SCREEN) * GRID;
-            int posX = 0; 
+            int posX = 0;
             using (Bitmap map = new Bitmap(width, height)) {
                 using (Graphics g = Graphics.FromImage(map)) {
-                    g.FillRectangle(Brushes.GhostWhite, 0, 0, SCREEN * GRID, height); // Denota área inicial (padrão)
-                    g.FillRectangle(Brushes.WhiteSmoke, SCREEN * GRID, 0, width, height); // Denota área gerada pela música
-                    g.FillRectangle(Brushes.GhostWhite, width - (SCREEN * GRID), 0, width, height); // Denota área final (padrão)
-
+                    DrawBackGround(g, height, heights.Count - 4);
                     foreach (var h in heights) {
-                        g.FillRectangle(Brushes.ForestGreen, posX * GRID, height - (h * GRID), MEASURE * GRID, h * GRID);
+                        DrawPlatform(g, posX, h, height);
                         posX += MEASURE;
                     }
-
-                    map.Save(name + ".png", ImageFormat.Png);
+                    DrawGridOverlay(g, width, height);
+                    DrawMeasureBounds(g, width, height);
+                    DrawScreenBounds(g, width, height);
+                    map.Save(name + "[div][0.1.6].png", ImageFormat.Png);
                 }
             }
         }
+
+        public static void DrawBackGround(Graphics graphics, int height, int musicWidth) {
+            graphics.FillRectangle(Brushes.MistyRose, 0, 0, SCREEN * GRID, height); // Denota área inicial (padrão)
+            graphics.FillRectangle(Brushes.Lavender, SCREEN * GRID, 0, musicWidth * MEASURE * GRID, height); // Denota área gerada pela música
+            graphics.FillRectangle(Brushes.MistyRose, musicWidth * MEASURE * GRID + SCREEN * GRID, 0, SCREEN * GRID, height); // Denota área final (padrão)
+        }
+
+        public static void DrawPlatform(Graphics graphics, int X, int platformHeight, int mapHeight) {
+            graphics.FillRectangle(Brushes.ForestGreen, X * GRID, mapHeight - (platformHeight * GRID), MEASURE * GRID, platformHeight * GRID);
+        }
+
+        public static void DrawScreenBounds(Graphics graphics, int mapWidth, int mapHeight) {
+            int X = 0;
+            while (X <= mapWidth) {
+                graphics.DrawLine(new Pen(Brushes.DarkBlue, 2), X, 0, X, mapHeight);
+                X += SCREEN * GRID;
+            }
+        }
+
+        public static void DrawMeasureBounds(Graphics graphics, int mapWidth, int mapHeight) {
+            int X = 0;
+            while (X <= mapWidth) {
+                graphics.DrawLine(new Pen(Brushes.DarkRed, 2), X, 0, X, mapHeight);
+                X += MEASURE * GRID;
+            }
+        }
+
+        public static void DrawGridOverlay(Graphics graphics, int mapWidth, int mapHeight) {
+            Pen line = new Pen(Brushes.Black, 1);
+            // Linhas Verticais
+            int X = 0;
+            while (X <= mapWidth) {
+                graphics.DrawLine(line, X, 0, X, mapHeight);
+                X += GRID;
+            }
+
+            // Linhas Horizontais
+            int Y = 0;
+            while (Y <= mapHeight) {
+                graphics.DrawLine(line, 0, Y, mapWidth, Y);
+                Y += GRID;
+            }
+        }
+
     }
 }
